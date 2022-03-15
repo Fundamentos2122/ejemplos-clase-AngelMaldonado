3c3203afbba18ed01b98fb32ded3892aeb3bebb4
AngelMaldonado <amaldonadojuarez@gmail.com>
2022-03-14 18:45:06 -0600

    3/14/22 Javascript

---
+++ JS/script.js
@@ -0,0 +1,21 @@
+/******************************************
+ *  Author : Angel Maldonado   
+ *  Created On : Mon Mar 14 2022
+ *  File : script.js
+ *******************************************/
+
+var header = document.getElementById("header");
+
+console.log(header);
+
+var parrafos = document.getElementsByTagName("p");
+
+console.log(parrafos);
+
+var p1 = document.getElementsByClassName("p1");
+
+console.log(p1);
+
+var p_x = document.querySelector("[type='x']");
+
+console.log(p_x);
